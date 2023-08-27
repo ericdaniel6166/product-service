97ed9925afb0ef69b65692de4e5694751c015fd7
eric6166 <ericnguyen6166@gmail.com>
2023-08-27 15:29:02 +0700

    feature/SM-0007

--- src/main/java/com/example/productservice/model/Product.java
+++ src/main/java/com/example/productservice/model/Product.java
@@ -1,23 +1,31 @@
 package com.example.productservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Document(value = "product")
+@Entity
+@Table(name = "p_products")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Data
 public class Product {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
+
 }

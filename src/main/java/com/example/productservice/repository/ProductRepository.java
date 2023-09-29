845a68f8daae30c79f4be581c498e6758f323915
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-09-30 00:54:11 +0700

    feature/SM-0025 (#18)

--- src/main/java/com/example/productservice/repository/ProductRepository.java
+++ src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.example.productservice.repository;
 
 import com.example.productservice.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAll(Pageable pageable);
+
+
 }
--- src/main/java/com/example/productservice/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/productservice/service/impl/ProductServiceImpl.java
@@ -1,17 +1,29 @@
 package com.example.productservice.service.impl;
 
-import com.example.productservice.dto.ProductRequest;
-import com.example.productservice.dto.ProductResponse;
+import com.example.productservice.dto.CreateProductRequest;
+import com.example.productservice.dto.ProductDto;
 import com.example.productservice.model.Product;
 import com.example.productservice.repository.ProductRepository;
 import com.example.productservice.service.ProductService;
+import com.example.springbootmicroservicesframework.pagination.AppPageRequest;
+import com.example.springbootmicroservicesframework.pagination.AppSortOrder;
+import com.example.springbootmicroservicesframework.pagination.MultiSortPageRequest;
+import com.example.springbootmicroservicesframework.utils.Const;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,15 +32,17 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
+    final ProductRepository productRepository;
+
+    final ModelMapper modelMapper;
 
     @Transactional
     @Override
-    public void create(ProductRequest productRequest) {
+    public void create(CreateProductRequest request) {
         Product product = Product.builder()
-                .name(productRequest.getName())
-                .description(productRequest.getDescription())
-                .price(productRequest.getPrice())
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
                 .build();
 
         productRepository.saveAndFlush(product);
@@ -36,18 +50,33 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<ProductResponse> getProducts() {
-        List<Product> products = productRepository.findAll();
-
-        return products.stream().map(this::mapToProductResponse).toList();
+    public PageImpl<ProductDto> findAll(Integer page, Integer size, Sort sort) {
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Product> result = productRepository.findAll(pageable);
+        List<ProductDto> productDto = result.stream().map(product -> modelMapper.map(product, ProductDto.class)).toList();
+        return new PageImpl<>(productDto,
+                PageRequest.of(page + 1, size, sort),
+                result.getTotalElements());
     }
 
-    private ProductResponse mapToProductResponse(Product product) {
-        return ProductResponse.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .build();
+    @Override
+    public PageImpl<ProductDto> findAllSortMultiColumn(MultiSortPageRequest request) {
+        List<AppSortOrder> orderList = request.getOrderList();
+        List<Sort.Order> orders = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(orderList)) {
+            orders = orderList.stream().map(AppSortOrder::mapToSortOrder).toList();
+        }
+        Sort.Order defaultOrder = new Sort.Order(Sort.Direction.ASC, Const.ID);
+        orders.add(defaultOrder);
+        return findAll(request.getPageNumber() - 1, request.getPageSize(), Sort.by(orders));
+    }
+
+    @Override
+    public PageImpl<ProductDto> findAll(AppPageRequest request) {
+        return findAll(request.getPageNumber() - 1,
+                request.getPageSize(),
+                Sort.by(Sort.Direction.fromString(request.getSortDirection()),
+                        request.getSortColumn()));
     }
 }
+

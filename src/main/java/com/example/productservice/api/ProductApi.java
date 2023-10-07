7a8a568f71a54c25519317ca499da790ecd899b9
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-10-07 18:39:05 +0700

    feature/SM-0028 (#21)

--- src/main/java/com/example/productservice/api/ProductApi.java
+++ src/main/java/com/example/productservice/api/ProductApi.java
@@ -6,12 +6,13 @@ import com.example.productservice.dto.CursorProductDto;
 import com.example.productservice.dto.ProductDto;
 import com.example.productservice.dto.UpdateProductRequest;
 import com.example.productservice.service.ProductService;
+import com.example.springbootmicroservicesframework.config.pagination.AppPageRequest;
+import com.example.springbootmicroservicesframework.config.pagination.CursorPageRequest;
+import com.example.springbootmicroservicesframework.config.pagination.CursorPageResponse;
+import com.example.springbootmicroservicesframework.config.pagination.MultiSortPageRequest;
 import com.example.springbootmicroservicesframework.dto.IdListResponse;
 import com.example.springbootmicroservicesframework.exception.NotFoundException;
-import com.example.springbootmicroservicesframework.pagination.AppPageRequest;
-import com.example.springbootmicroservicesframework.pagination.CursorPageRequest;
-import com.example.springbootmicroservicesframework.pagination.CursorPageResponse;
-import com.example.springbootmicroservicesframework.pagination.MultiSortPageRequest;
+import com.example.springbootmicroservicesframework.utils.Const;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -20,6 +21,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -41,39 +44,40 @@ import javax.validation.constraints.NotNull;
 public class ProductApi {
     final ProductService productService;
 
-    @GetMapping("{id}")
-    public ResponseEntity<ProductDto> getById(@PathVariable @NotNull @Min(value = 1) Long id) throws NotFoundException {
-        log.info("findById"); //delete
-        return ResponseEntity.ok(productService.getById(id));
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductDto> deleteById(@PathVariable @NotNull @Min(value = 1)
+                                                 @Max(value = Const.DEFAULT_MAX_LONG) Long id)
+            throws NotFoundException {
+        productService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> findById(@PathVariable @NotNull @Min(value = 1)
+                                               @Max(value = Const.DEFAULT_MAX_LONG) Long id)
+            throws NotFoundException {
+        return ResponseEntity.ok(productService.findById(id));
     }
 
     @PutMapping
     public ResponseEntity<IdListResponse> update(@RequestBody @Valid UpdateProductRequest request) throws NotFoundException {
-        System.out.println(request.toString()); //delete
-        log.info("update"); //delete
         return ResponseEntity.ok(productService.update(request));
     }
 
 
     @PostMapping
     public ResponseEntity<IdListResponse> create(@RequestBody @Valid CreateProductRequest request) {
-        System.out.println(request.toString()); //delete
-        log.info("create"); //delete
-        return ResponseEntity.ok(productService.saveAndFlush(request));
+        return ResponseEntity.ok(productService.create(request));
     }
 
 
-    @PostMapping("create-multi")
+    @PostMapping("/create-multi")
     public ResponseEntity<IdListResponse> createMulti(@RequestBody @Valid CreateMultiProductRequest request) {
-        System.out.println(request.toString()); //delete
-        log.info("createMulti"); //delete
-        return ResponseEntity.ok(productService.saveAllAndFlush(request));
+        return ResponseEntity.ok(productService.createMulti(request));
     }
 
     @GetMapping
     public ResponseEntity<PageImpl<ProductDto>> findAll(@Valid AppPageRequest request) {
-        System.out.println(request.toString()); //delete
-        log.info("findAll"); //delete
         PageImpl<ProductDto> response = productService.findAll(request);
         if (!response.hasContent()) {
             return ResponseEntity.noContent().build();
@@ -83,9 +87,6 @@ public class ProductApi {
 
     @PostMapping("/sort-multi-column")
     public ResponseEntity<PageImpl<ProductDto>> findAllSortMultiColumn(@RequestBody @Valid MultiSortPageRequest request) {
-        System.out.println(request.toString()); //delete
-        log.info("findAllSortMultiColumn"); //delete
-
         PageImpl<ProductDto> response = productService.findAllSortMultiColumn(request);
         if (!response.hasContent()) {
             return ResponseEntity.noContent().build();
@@ -94,10 +95,8 @@ public class ProductApi {
     }
 
     @GetMapping("/cursor-pagination")
-    public ResponseEntity<CursorPageResponse<CursorProductDto>> findAllCursorPagination(@Valid CursorPageRequest request) throws IllegalAccessException {
-        System.out.println(request.toString()); //delete
-        log.info("findAllCursorPagination"); //delete
-
+    public ResponseEntity<CursorPageResponse<CursorProductDto>> findAllCursorPagination(@Valid CursorPageRequest request)
+            throws IllegalAccessException {
         CursorPageResponse<CursorProductDto> response = productService.findAllCursorPagination(request);
         if (CollectionUtils.isEmpty(response.getContent())) {
             return ResponseEntity.noContent().build();
--- src/main/java/com/example/productservice/service/ProductService.java
+++ src/main/java/com/example/productservice/service/ProductService.java
@@ -5,18 +5,18 @@ import com.example.productservice.dto.CreateProductRequest;
 import com.example.productservice.dto.CursorProductDto;
 import com.example.productservice.dto.ProductDto;
 import com.example.productservice.dto.UpdateProductRequest;
+import com.example.springbootmicroservicesframework.config.pagination.AppPageRequest;
+import com.example.springbootmicroservicesframework.config.pagination.CursorPageRequest;
+import com.example.springbootmicroservicesframework.config.pagination.CursorPageResponse;
+import com.example.springbootmicroservicesframework.config.pagination.MultiSortPageRequest;
 import com.example.springbootmicroservicesframework.dto.IdListResponse;
 import com.example.springbootmicroservicesframework.exception.NotFoundException;
-import com.example.springbootmicroservicesframework.pagination.AppPageRequest;
-import com.example.springbootmicroservicesframework.pagination.CursorPageRequest;
-import com.example.springbootmicroservicesframework.pagination.CursorPageResponse;
-import com.example.springbootmicroservicesframework.pagination.MultiSortPageRequest;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Sort;
 
 public interface ProductService {
 
-    IdListResponse saveAndFlush(CreateProductRequest request);
+    IdListResponse create(CreateProductRequest request);
 
     PageImpl<ProductDto> findAll(Integer page, Integer size, Sort sort);
 
@@ -26,9 +26,12 @@ public interface ProductService {
 
     CursorPageResponse<CursorProductDto> findAllCursorPagination(CursorPageRequest request) throws IllegalAccessException;
 
-    IdListResponse saveAllAndFlush(CreateMultiProductRequest request);
+    IdListResponse createMulti(CreateMultiProductRequest request);
 
     IdListResponse update(UpdateProductRequest request) throws NotFoundException;
 
-    ProductDto getById(Long id) throws NotFoundException;
+    ProductDto findById(Long id) throws NotFoundException;
+
+    void deleteById(Long id) throws NotFoundException;
 }
+
